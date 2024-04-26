4770a0beaa99047d9cb545b6db7020510f677f04
Fredrik Haarde <fredrik.haarde@bouvet.no>
2024-04-26 10:24:14 +0200

    Implementert interface

--- AzureServices.Table/ITableService.cs
+++ AzureServices.Table/ITableService.cs
@@ -1,12 +1,35 @@
 ﻿using Azure.Data.Tables;
+using System.Linq.Expressions;
 
 namespace AzureServices.Table;
 
 public interface ITableService
 {
-    /// <summary>
-    /// The <see cref="GetServiceClient"/> method returns the underlying <see cref="TableServiceClient"/>.
-    /// </summary>
-    /// <returns>The underlying <see cref="TableServiceClient"/> instance.</returns>
+    void AddEntitiesToTable<T>(string tableName, IEnumerable<T> entities) where T : class, ITableEntity, new();
+
+    static void AddEntitiesToTable<T>(TableClient table, IEnumerable<T> entities) where T : class, ITableEntity, new()
+    {
+        foreach (var entity in entities)
+        {
+            table.AddEntity(entity);
+        }
+    }
+
+    void AddEntityToTable<T>(string tableName, T entity) where T : class, ITableEntity, new();
+
+    static void AddEntityToTable<T>(TableClient table, T entity) where T : class, ITableEntity, new()
+    {
+        table.AddEntity(entity);
+    }
+
     TableServiceClient GetServiceClient();
+
+    TableClient GetTable(string tableName, bool createIfNotExists = false);
+
+    List<T> QueryTable<T>(string tableName, Expression<Func<T, bool>> query) where T : class, ITableEntity, new();
+
+    static List<T> QueryTable<T>(TableClient table, Expression<Func<T, bool>> query) where T : class, ITableEntity, new()
+    {
+        return table.Query(query).ToList();
+    }
 }
